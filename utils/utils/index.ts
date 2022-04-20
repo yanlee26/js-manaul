e4de12e0d120236952b8545acd80232d803120c5
li <yanli@onlyedu.com>
2022-04-20 14:03:23 +0800

    feat: add utils

--- utils/utils/index.ts
+++ utils/utils/index.ts
@@ -1,17 +1,10 @@
 export * from './time'
 export * from './proxy'
-export * from './converter'
 export * from './copy'
-export * from './request'
 export * from './rules'
-export * from './utils'
 export * from './prefix'
 export * from './modal'
 export * from './url'
-export * from './storage'
-export * from './string_encryption'
-export * from './autoIncreateId'
-export * from './initMathJax'
 
 export function wait(seconds: number) {
   return new Promise(resolve => {
@@ -26,4 +19,21 @@ export function countChangedTimes(array: Array<number | string | boolean>) {
     }
     return re
   }, 0)
+}
+
+export async function patchAPICatch({ params, type = '', api, onSuccess }: patchCatchParams) {
+  // 一般错误统一抛出
+  try {
+    const { success, data } = await api(params)
+    if (success === false) {
+      // message.error(`${type}失败！`)
+    } else {
+      type && message.success(`${type}成功！`)
+      if (onSuccess) {
+        onSuccess(data)
+      }
+    }
+  } catch (e) {
+    console.error(`${type}失败！`)
+  }
 }
